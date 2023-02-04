0d27f5ef97ab6704e94d1a29ab5a5b7670460b50
Stephen Rudge <sbrudge@comcast.net>
2023-02-03 19:32:00 -0600

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System.Linq;
+
+namespace MaskSensitive
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please Enter a Secret Code");
+            string code = Console.ReadLine();
+
+
+            if (code.Length > 4)
+            {
+                string v = code[^4..].PadLeft(code.Length, '*');
+                var DisplayText = v;
+                Console.WriteLine($"{DisplayText}");
+                Console.ReadLine();
+            }
+        }
+    }
+}
